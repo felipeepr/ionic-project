3297ddfbd3c238c659bdd5ba0095a1a8b8270501
Pipe <prietofelipe51@gmail.com>
2025-08-27 17:03:41 -0500

    Uploading project

---
+++ src/app/pages/my-profile/my-profile.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.page.html',
+  styleUrls: ['./my-profile.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class MyProfilePage implements OnInit {
+
+  constructor() { }
+
+  title : string = "MY PROFILE";
+  name : string = "Felipe";
+  lastName : string = "Prieto";
+  mail : string = "prietofelipe51@gmail.com";
+  birthDate : Date = new Date(2002, 8, 18);
+  jobTitle : string = "Support TI Analyst";
+  ngOnInit() {
+  }
+
+}
